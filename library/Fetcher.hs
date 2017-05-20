c2ad51ffba8f87d8c18dc31728350dd2b0dabc90
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-05-20 20:09:43 -0300

    Fetcher should log HTTP requests duration

--- library/Fetcher.hs
+++ library/Fetcher.hs
@@ -2,8 +2,14 @@
 
 module Fetcher where
 
+import           Data.Aeson
 import qualified Data.ByteString.Char8 as C8
+import qualified Data.Text.Lazy        as T
+import           Formatting
+import           Formatting.Clock
+import qualified Logger
 import           Network.HTTP.Simple
+import           System.Clock
 import qualified User
 import qualified Wunderlist.List
 import qualified Wunderlist.Task
@@ -13,6 +19,17 @@ listsRequest = "GET http://a.wunderlist.com/api/v1/lists"
 
 tasksRequest :: Request
 tasksRequest = "GET http://a.wunderlist.com/api/v1/tasks"
+
+-- Works like httpJSON, but logs request duration from start to finish.
+httpJSONLogged :: FromJSON a => Request -> String -> IO (Response a)
+httpJSONLogged request description = do
+  start <- getTime Monotonic
+  let info = T.pack description
+  Logger.log (sformat ("Started " % text % ".") info)
+  response <- httpJSON request
+  end <- getTime Monotonic
+  Logger.log (sformat ("Finished " % text % " after " % timeSpecs % ".") info start end)
+  return response
 
 fillRequest :: User.User -> Request -> Request
 fillRequest user request = withEverything
@@ -25,7 +42,7 @@ fillRequest user request = withEverything
 fetchLists :: User.User -> IO [Wunderlist.List.List]
 fetchLists user = do
   let request = fillRequest user listsRequest
-  response <- httpJSON request
+  response <- httpJSONLogged request "fetching all lists"
   return (getResponseBody response :: [Wunderlist.List.List])
 
 fetchInbox :: User.User -> IO Wunderlist.List.List
@@ -41,5 +58,5 @@ fetchTasks user list = do
   let baseRequest = fillRequest user tasksRequest
   let listId = Just (C8.pack (show (Wunderlist.List.id list)))
   let request = setRequestQueryString [("list_id", listId)] baseRequest
-  response <- httpJSON request
+  response <- httpJSONLogged request "fetching tasks"
   return (getResponseBody response :: [Wunderlist.Task.Task])
