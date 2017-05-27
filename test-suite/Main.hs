1a2060565705a38ff53e3e5fee4ab4e5af37f321
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-05-27 12:35:54 -0300

    Log trimming should never corrupt lines

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import           Control.Monad
 import qualified Data.ByteString           as B
 import           Logger
@@ -13,17 +15,28 @@ newtype ACOff = ACOff COff
   deriving (Show)
 
 instance Arbitrary ACOff where
-  arbitrary = ACOff `liftM` (liftM (fromIntegral :: Int -> COff) (choose (0, 100)))
+  arbitrary = ACOff `liftM` (liftM (fromIntegral :: Int -> COff) (choose (0, 127)))
 
 unACOff :: ACOff -> COff
 unACOff (ACOff off) = off
 
 trimByteStringProperties :: [TestTree]
 trimByteStringProperties =
-  [QC.testProperty "upper limit is respected" trimByteStringShouldRespectTheUpperLimit]
+  [ QC.testProperty "upper limit is respected" trimByteStringShouldRespectTheUpperLimit
+  , QC.testProperty "no line is corrupted" trimByteStringShouldNotCorruptLines
+  ]
 
 trimByteStringShouldRespectTheUpperLimit :: B.ByteString -> ACOff -> Bool
 trimByteStringShouldRespectTheUpperLimit b s = trimmedSize <= asCOff
   where
     asCOff = unACOff s
     trimmedSize = fromIntegral (B.length (Logger.trimByteString b asCOff)) :: COff
+
+trimByteStringShouldNotCorruptLines :: B.ByteString -> ACOff -> Bool
+trimByteStringShouldNotCorruptLines b s = trimmedLines == expectedLines
+  where
+    asCOff = unACOff s
+    newline = head (B.unpack "\n")
+    sourceLines = B.split newline b
+    trimmedLines = B.split newline (Logger.trimByteString b asCOff)
+    expectedLines = drop (length sourceLines - length trimmedLines) sourceLines
